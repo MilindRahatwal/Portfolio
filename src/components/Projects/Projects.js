fb5200dd66a28fe043a2241c042b464708998801
Milind Rahatwal <113734653+MilindRahatwal@users.noreply.github.com>
2023-09-18 20:51:37 -0300

    Add files via upload

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,9 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Gene expression analysis for the diagnosis of Type 2 Diabetes"
+              description="In this project, we predict T2D with the help of gene expression. T2D is mostly attributed to the failure of pancreatic islet cells. The processing of data in a meaningful and beneficial manner is now possible thanks to modern technologies.The identification of distinctive transcriptional signatures has been made feasible by single-cell RNA sequencing (scRNA-seq)."
+              ghLink="https://github.com/milindrahatwal/"
             />
           </Col>
 
@@ -36,10 +35,9 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Toxicity Prediction"
+              description="The competition was hosted to kaggle to predict if the chemicals are toxic or not. For this competition we were given two datasets: a train dataset which had two columns Id and Expected and a test dataset which had a single column named x. The goal of the competition was to determine which chemicals are toxic with the help of machine learning."
+              ghLink="https://github.com/milindrahatwal/Kaggle-Toxicity-Prediction-II"
             />
           </Col>
 
@@ -47,10 +45,9 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Profile Based Agriculture System"
+              description="A soil profile based crop prediction system that recommends the crop to be grown based on soil data. It also detects diseases of infected crop from the uploaded images and suggests remedies."
+              ghLink="https://github.com/milindrahatwal/"            
             />
           </Col>
 
@@ -60,8 +57,7 @@ function Projects() {
               isBlog={false}
               title="Plant AI"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/milindrahatwal/"
             />
           </Col>
 
@@ -69,24 +65,12 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Location Based Security"
+              description="A moibile application that uses a location dependent approach to share files to registered users. It also offers twop factor authentication and sends user an OTP before making the file accessible. The file key is encrypted using Advanced Encryption System before sharing."
+              ghLink="https://github.com/milindrahatwal/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
